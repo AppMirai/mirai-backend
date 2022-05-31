f57483583da680755970f44f8997fa630f05b271
Devafilla Rizqy S <devafillar.s@gmail.com>
2022-05-31 09:52:05 +0700

    fix resource

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -15,17 +15,19 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'image' => $this->image,
-            'price' => $this->price,
-            'shade' => $this->shade,
-            'description' => $this->description,
-            'tokopedia' => $this->tokopedia,
-            'shopee' => $this->shopee,
-            'images' => $this->images,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'data' => [
+                'id' => $this->id,
+                'name' => $this->name,
+                'image' => $this->image,
+                'price' => $this->price,
+                'shade' => $this->shade,
+                'description' => $this->description,
+                'tokopedia' => $this->tokopedia,
+                'shopee' => $this->shopee,
+                'images' => $this->images,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ]
         ];
     }
 }
