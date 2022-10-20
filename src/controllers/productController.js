5f7a6284e269754c586a885d54cc1a3b9791b579
Raihan Ahnaf <mz.raihan11@gmail.com>
2022-10-20 13:51:23 +0700

    fix: bug on product controller

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -19,7 +19,6 @@ module.exports = {
                 },
             });
 
-            console.log(check_like == null ? false : true);
             res.status(200).json({ message: 'success retreived data!', data: products });
         } catch (error) {
             res.status(500).send(error.message);
